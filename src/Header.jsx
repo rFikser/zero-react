99efa1051048afcf72fc1c0a71bfd9afb8502a7a
rFikser <john321cheba@gmail.com>
2020-06-07 16:30:34 +0300

    webpack server with css modules

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./header.css";
+
+export function Header() {
+  return (
+    <header>
+      <h1 className={styles.example}>Reddit for our own v. 2.0</h1>
+    </header>
+  );
+}
